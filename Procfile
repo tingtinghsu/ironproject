4096872329ba7b057f9dc56b2dfc164ecf18d62a
tingtinghsu <tingtinghsu.tw@gmail.com>
2022-10-09 17:27:38 +0800

    Day25

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/ironproject.jar clojure.main -m ironproject.server
---
+++ env/prod/cljs/ironproject/prod.cljs
@@ -0,0 +1,7 @@
+(ns ironproject.prod
+  (:require [ironproject.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
